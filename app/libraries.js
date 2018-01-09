25c12d6441779e2f93303c68793e5ec6b6ee0649
Raja Jamwal <raja@mobikontech.com>
2018-01-09 16:28:43 +0530

    lodash & material-design-icons support

--- app/libraries.js
+++ app/libraries.js
@@ -3,7 +3,7 @@
  */
 window.jQuery = window.$ = require("node_modules/jquery/dist/jquery.js");
 
-// window.$.velocity = require("node_modules/velocity-animate/velocity.js");
+window._ = require("lodash");
 
 require("bower_components/angular/angular.js");
 require("bower_components/angular-ui-router/release/angular-ui-router.js");
@@ -12,3 +12,5 @@ require("bower_components/angular-material/angular-material.css");
 require("bower_components/angular-aria/angular-aria.js");
 require("bower_components/angular-animate/angular-animate.js");
 require("bower_components/angular-material/angular-material.js");
+
+require("node_modules/material-design-icons/iconfont/material-icons.css");
